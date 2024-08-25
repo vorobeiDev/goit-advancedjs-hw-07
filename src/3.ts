902c8c0fb4740c488f0999dc44be7c3bf8755d89
Vlad Vorobei <vorobei.dev@gmail.com>
2024-08-25 11:54:49 +0300

    Finished homework

--- src/3.ts
+++ src/3.ts
@@ -2,10 +2,21 @@
   Ви створюєте гру, де є персонажі з різними ролями.
   Зараз ви працюєте над класом Wizard, який має реалізовувати два інтерфейси - ICharacter та ISpellCaster.
 
-  Визначте інтерфейси ICharacter та ISpellCaster так, щоб вони відповідали вимогам класу Wizard. 
-  Інтерфейс ICharacter повинен включати властивості name і level, і навіть метод introduce і levelUp. 
+  Визначте інтерфейси ICharacter та ISpellCaster так, щоб вони відповідали вимогам класу Wizard.
+  Інтерфейс ICharacter повинен включати властивості name і level, і навіть метод introduce і levelUp.
   Інтерфейс ISpellCaster повинен включати метод castSpell.
 */
+
+interface ICharacter {
+  name: string;
+  level: number;
+  introduce(phrase: string): void;
+  levelUp(): void;
+}
+
+interface ISpellCaster {
+  castSpell(): void;
+}
 
 // реалізація класу Wizard
 class Wizard implements ICharacter, ISpellCaster {
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,56 @@
+class Key {
+  private readonly signature: number
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): number {
+    return this.key.getSignature();
+  }
+}
+
+abstract class House {
+  protected readonly tenants: Person[] = [];
+  protected door: boolean;
+
+  protected constructor(public key: Key) {}
+
+  public comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('The person is coming in...');
+    } else {
+      console.log('The door is closed');
+    }
+  }
+
+  public openDoor(key: number) {}
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: number) {
+    if (key === this.key.getSignature()) {
+      this.door = true;
+      console.log('The door is opened');
+    } else {
+      console.log('Wrong key');
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +59,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
